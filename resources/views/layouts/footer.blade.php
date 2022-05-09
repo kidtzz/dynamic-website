f28b0bd2c81135ace8823e4d88e424941435cc8a
kidtzz <bangkitpr4@gmail.com>
2022-05-09 14:06:34 +0700

    update

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="container">
+    <h5 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing.</h5>
+</div>
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@push('addon-style')
+@endpush
+
+@section('content')
+    <div class="container">
+        <div class="col-lg-6">
+            <h1>jhehe</h1>
+        </div>
+    </div>
+@endsection
